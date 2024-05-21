97ffb19179a85a6fc4e0cbd913b2f35c2937df3d
FrenchFry36 <134784793+FrenchFry36@users.noreply.github.com>
2024-05-21 09:40:59 +0200

    02 file created

--- 02_Numeric_Data_Types/num_data_tp.py
+++ 02_Numeric_Data_Types/num_data_tp.py
@@ -1,3 +1,15 @@
 # implement print() function to see the message in terminal 
 
 print("Python has three numeric types: int, float, and complex")
+
+# variable assigned
+myValue=1
+
+# print the assigned variable
+print(myValue)
+
+# get the data type of the variable
+print(type(myValue))
+
+# converting to the string
+print(str(myValue) + " is of the data type " + str(type(myValue)))
