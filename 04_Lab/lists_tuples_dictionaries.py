3ef1b874c4eafeea9ae8e8eea7fa006eaa7a7242
FrenchFry36 <134784793+FrenchFry36@users.noreply.github.com>
2024-05-21 10:46:18 +0200

    04 file is created

---
+++ 04_Lab/lists_tuples_dictionaries.py
@@ -0,0 +1,46 @@
+# creating a list of fruits
+myFruitList = ["apple", "banana", "cherry"]
+
+# print the values and types of the list
+print(myFruitList)
+print(type(myFruitList))
+
+# print 1st value
+print(myFruitList[0])
+
+# print 2nd value
+print(myFruitList[1])
+
+# print 3rd value
+print(myFruitList[2])
+
+# print the values
+print(myFruitList)
+
+# creating a tuple and printing
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+# print 1st value
+print(myFinalAnswerTuple[0])
+
+# print 2nd value
+print(myFinalAnswerTuple[1])
+
+# print 3rd value
+print(myFinalAnswerTuple[2])
+
+# creating a dictionary and printing
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+# print everyone's favorite fruit
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
