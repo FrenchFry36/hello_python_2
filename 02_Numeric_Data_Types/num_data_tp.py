14b155c18db30d7389c1f448f5b8177609e64e2f
FrenchFry36 <134784793+FrenchFry36@users.noreply.github.com>
2024-05-21 09:59:26 +0200

    num_data_tp is edited

--- 02_Numeric_Data_Types/num_data_tp.py
+++ 02_Numeric_Data_Types/num_data_tp.py
@@ -11,5 +11,53 @@ print(myValue)
 # get the data type of the variable
 print(type(myValue))
 
-# converting to the string
+# converting the value of the var to the string
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# reassigning the variable to float type
+myValue=3.14
+
+# prin the var
+print(myValue)
+
+# retrive the type of var
+print(type(myValue))
+
+# converting the value of the var to the string
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# reassigning the var to complex type
+myValue=5j
+
+# prin the var
+print(myValue)
+
+# retrive the type of var
+print(type(myValue))
+
+# converting the value of the var to the string
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# reassigning the var the boolean True
+myValue=True
+
+# prin the var
+print(myValue)
+
+# retrive the type of var
+print(type(myValue))
+
+# converting the value of the var to the string
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# reassigning the var the boolean False
+myValue=False
+
+# prin the var
+print(myValue)
+
+# retrive the type of var
+print(type(myValue))
+
+# converting the value of the var to the string
 print(str(myValue) + " is of the data type " + str(type(myValue)))
